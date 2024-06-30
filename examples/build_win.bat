7cbe782a057b0a5d6d1118f6f36cb7d7c634ea1b
Rafael Henrique Capati <rhcapati@gmail.com>
2024-06-30 11:40:40 -0300

    Improve build scripts - Print some information about target and mode - Makefile: declares all targets as phony that don't represent actual files and remove the suffix _example
    Former-commit-id: aea125736b1d3328975142af53db3740110c9d8c

--- examples/build_win.bat
+++ examples/build_win.bat
@@ -1,87 +1,71 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
 :: Set default values
 set ARGS=-debug -vet -strict-style
+set RELEASE_MODE=false
+set BUILD_TARGET=
 
-:: Arguments optimized for release
-if "%2" == "release" (
-	set ARGS=-o:speed ^
-		-disable-assert ^
-		-no-bounds-check ^
-		-define:WGPU_CHECK_TO_BYTES=false ^
-		-define:WGPU_ENABLE_ERROR_HANDLING=false
+:: Check all arguments for "release" and identify the build target
+for %%i in (%*) do (
+    if /i "%%i"=="release" (
+        set RELEASE_MODE=true
+    ) else if "!BUILD_TARGET!"=="" (
+        set BUILD_TARGET=%%i
+    )
 )
 
+:: Set mode string
+if "%RELEASE_MODE%"=="true" (
+    set MODE=RELEASE
+) else (
+    set MODE=DEBUG
+)
+
+if "%RELEASE_MODE%"=="true" (
+    set ARGS=-o:speed ^
+        -disable-assert ^
+        -no-bounds-check ^
+        -define:WGPU_CHECK_TO_BYTES=false ^
+        -define:WGPU_ENABLE_ERROR_HANDLING=false
+)
+
+:: Force DX12 backend
 ::set ARGS=%ARGS% -define:WGPU_BACKEND_TYPE=DX12
 
 set OUT=-out:.\build
 
-if "%1" == "" (
-    call :all & goto :end
+:: Process the build target
+if "%BUILD_TARGET%"=="" (
+    echo Building all examples in %MODE% mode...
+    call :all
+) else (
+    echo Building '%BUILD_TARGET%' in %MODE% mode...
+    call :%BUILD_TARGET%
+    if errorlevel 1 (
+        echo Example not recognized: %BUILD_TARGET%
+    )
 )
-
-if "%1" == "info" (
-    call :info & goto :end
-)
-
-if "%1" == "triangle" (
-    call :triangle & goto :end
-)
-
-if "%1" == "triangle_msaa" (
-    call :triangle_msaa & goto :end
-)
-
-if "%1" == "cube" (
-    call :cube & goto :end
-)
-
-if "%1" == "cube_textured" (
-    call :cube_textured & goto :end
-)
-
-if "%1" == "compute" (
-    call :compute & goto :end
-)
-
-if "%1" == "capture" (
-    call :capture & goto :end
-)
-
-if "%1" == "learn_wgpu" (
-    call :learn_wgpu & goto :end
-)
-
-if "%1" == "texture_arrays" (
-    call :texture_arrays & goto :end
-)
-
-echo Example not recognized.
 goto :end
 
 :all
-call :info
-call :triangle
-call :triangle_msaa
+call :capture
+call :compute
 call :cube
 call :cube_textured
-call :compute
-call :capture
-call :learn_wgpu
+call :info
 call :texture_arrays
+call :triangle
+call :triangle_msaa
+call :learn_wgpu
 goto :eof
 
-:info
-odin build .\info %ARGS% %OUT%\info.exe
+:capture
+odin build .\capture %ARGS% %OUT%\capture.exe
 goto :eof
 
-:triangle
-odin build .\triangle %ARGS% %OUT%\triangle.exe
-goto :eof
-
-:triangle_msaa
-odin build .\triangle_msaa %ARGS% %OUT%\triangle_msaa.exe
+:compute
+odin build .\compute %ARGS% %OUT%\compute.exe
 goto :eof
 
 :cube
@@ -92,16 +76,20 @@ goto :eof
 odin build .\cube_textured %ARGS% %OUT%\cube_textured.exe
 goto :eof
 
-:compute
-odin build .\compute %ARGS% %OUT%\compute.exe
-goto :eof
-
-:capture
-odin build .\capture %ARGS% %OUT%\capture.exe
+:info
+odin build .\info %ARGS% %OUT%\info.exe
 goto :eof
 
 :texture_arrays
 odin build .\texture_arrays %ARGS% %OUT%\texture_arrays.exe
+goto :eof
+
+:triangle
+odin build .\triangle %ARGS% %OUT%\triangle.exe
+goto :eof
+
+:triangle_msaa
+odin build .\triangle_msaa %ARGS% %OUT%\triangle_msaa.exe
 goto :eof
 
 :learn_wgpu
